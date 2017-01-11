eb56d191b8c9180468a82dfe24d333d8c0217810
HecFranco <hector.franco.aceituno@gmail.com>
2017-01-11 22:33:16 +0100

    Accordion Effect
    There are two accordion effects, one of menu and other of elements

---
+++ Accordion effect/script.js
@@ -0,0 +1,27 @@
+//Sobre el elemento con clase accordion, escucha un click en el elemento h2
+$('.accordion').on('click','h2',function(){
+	var t = $(this);
+//busca el siguiente elemento
+	var tp = t.next();
+//dentro del padre busca un hermano p
+	var p = t.parent().siblings().find('p');
+//oculta el siguiente dentro del padre busca un hermano p (sin esta podría funcionar)
+	p.slideUp();
+//Muestra u oculta el siguiente elemento	
+	tp.slideToggle();
+
+});
+$('.accordion').on('click','.accordion_item',function(){
+	var t = $(this);
+	var tp = t.children('ul');
+	var p = t.parent().find('li>ul');
+	p.slideUp();
+	tp.slideToggle();
+
+});
+/*
+// con solo esta línea de código se abre el elemento y no se cerrarría al abrir el resto
+$('.accordion').on('click','h2',function(){
+	$(this).next().slideToggle(); 
+})
+*/
